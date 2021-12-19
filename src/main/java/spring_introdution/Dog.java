8dfeeedaa5cbaf80fc91f4588ebe612c9cf6ed9b
EminZalaev <95588733+EminZalaev@users.noreply.github.com>
2021-12-19 21:46:58 +0300

    сreated new test and context

--- src/main/java/spring_introdution/Dog.java
+++ src/main/java/spring_introdution/Dog.java
@@ -1,8 +1,30 @@
 package spring_introdution;
 
 public class Dog implements Pet{
+    //private String name;
+
+    /*public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }*/
+
+    public Dog() {
+        System.out.println("Gog bean is created");
+    }
     @Override
+
     public void say(){
         System.out.println("Wow-Wow");
     }
+    public void init(){
+        System.out.println("Method init");
+    }
+
+    public void destroy() {
+        System.out.println("Method destroyed");
+    }
+
 }
